9faf568b0822747195c64250a71581288bedbcf4
rafique687 <info.rafique687@gmail.com>
2025-07-21 11:06:00 +0530

    assignmentandarraygroup

--- arraygroup.js
+++ arraygroup.js
@@ -1,19 +1,14 @@
-let arr = ['India','China','Indonesia','Australia','Afganistan'];
-let narr ={};
-for(let i=0;i<arr.length;i++)
-{
-// if(narr.hasOwnProperty(arr[i][0]))
-  narr=arr[i][0];
-   narr  = [ arr[i] ];
+let arr = ['India', 'China', 'Indonesia', 'Australia', 'Afganistan'];
+let narr = {};
 
-    for(let j=i+1;j<arr.length;j++)
-    {  
-    if(arr[i][0]==arr[j][0] )
-    {
-      
-        narr.push(arr[j]);
+for (let i = 0; i < arr.length; i++) {
+    let firstLetter = arr[i][0];
+    
+    if (!narr[firstLetter]) {
+        narr[firstLetter] = []; 
     }
-        
-    }
-    console.log(narr);
+
+    narr[firstLetter].push(arr[i]); 
 }
+//document.writeln(narr);
+console.log(narr);
